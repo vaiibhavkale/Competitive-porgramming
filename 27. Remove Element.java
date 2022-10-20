d7e0ec163d928c887ad785912ef02810c6b23bfb
Vaibhav Kale <64345541+vaiibhavkale@users.noreply.github.com>
2022-10-21 01:05:48 +0530

    Create 27. Remove Element.java

---
+++ 27. Remove Element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+          int output = 0;
+
+          for(int i=0; i<nums.length; i++) {
+              if(nums[i] != val) {
+                  nums[output] = nums[i];
+                  output++;
+              }
+         }     
+         return output;      
+    }
+}
