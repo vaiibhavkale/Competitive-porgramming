a0025907c8a4c7e3b46e52f776e0ac2f58223c65
Vaibhav Kale <64345541+vaiibhavkale@users.noreply.github.com>
2022-10-27 09:42:36 +0530

    Create 70. Climbing Stairs.java

---
+++ 70. Climbing Stairs.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        if(n<=1) return 1;
+        
+        int prev1 = 1; 
+        int prev2 = 2;
+        
+        for(int i = 3; i<=n; i++){
+            int temp = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = temp;
+        }
+        return prev2;
+    }
+}
