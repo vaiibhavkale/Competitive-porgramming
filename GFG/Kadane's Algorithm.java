9cabcc8a386444aa481628a0107dc972106437c7
Vaibhav Kale <64345541+vaiibhavkale@users.noreply.github.com>
2024-11-25 13:30:13 +0530

    Create Kadane's Algorithm.java

---
+++ GFG/Kadane's Algorithm.java
@@ -0,0 +1,23 @@
+// User function Template for Java
+class Solution {
+
+    // arr: input array
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int[] arr) {
+        // Your code here
+        int max = Integer.MIN_VALUE; 
+        int sum = 0;
+        
+        for(int i = 0; i<arr.length; i++){
+            sum += arr[i];
+          
+            if(sum > max){
+                max = sum;
+            }
+            if(sum < 0){
+                sum = 0;
+            }
+        }
+        return max;
+    }
+}
