8c3cb825ef8c26dde26db90cbca498d5a3769077
Vaibhav Kale <64345541+vaiibhavkale@users.noreply.github.com>
2024-11-25 13:38:41 +0530

    Update Reverse an Array.java

--- GFG/Reverse an Array.java
+++ GFG/Reverse an Array.java
@@ -1,33 +1,12 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-class GFG {
-    
-     
-    public static void main(String args[]) {
-        Scanner sc=new Scanner(System.in);
-        int t=sc.nextInt();
+class Solution {
+    public void reverseArray(int arr[]) {
+        // code here
+        int temp, value;
         
-        while(t!=0){
-            int n=sc.nextInt();
-        
-            int[] a=new int[n];
-            
-            for (int i=0;i<n;i++){
-                a[i]=sc.nextInt();
-                
-            }
-            
-            for (int i=n-1;i>=0;i--){
-                System.out.print(a[i]+" ");
-            }
-            t--;
-            System.out.println(); //multiple test cases are available here that why for every other test case we have to print new line
-            
+        for(int i = 0; i < arr.length/2; i++){
+            temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
         }
-        
-        sc.close();
-        
     }
 }
